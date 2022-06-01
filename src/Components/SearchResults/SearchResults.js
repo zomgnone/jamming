ad3b7893966d6ba8fa5e9ee1f7bad86b40c7fd36
Andrew S <44890623+zomgnone@users.noreply.github.com>
2022-06-01 17:38:05 +0300

    Add React components

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,18 @@
+import './SearchResults.css'
+
+import React from 'react';
+
+import TrackList from '../TrackList/TrackList'
+
+class SearchResults extends React.Component {
+  render() {
+    return (
+      <div className="SearchResults">
+        <h2>Results</h2>
+        <TrackList tracks={this.props.searchResults}/>
+      </div>
+    );
+  }
+}
+
+export default SearchResults;
---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,23 @@
+import './TrackList.css';
+
+import React from 'react';
+
+import Track from '../Track/Track';
+
+
+class TrackList extends React.Component {
+  render() {
+    return(
+      <div className="TrackList">
+        {
+          this.props.tracks.map(track => {
+            return <Track track={track}
+                          key={track.id} />;
+          })
+        }
+      </div>
+    );
+  }
+}
+
+export default TrackList;
